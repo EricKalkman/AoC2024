42f8936703e82d1b545fe2a6360eb35bb2d2140a
Eric Kalkman <eric.kalkman@pm.me>
2024-11-29 17:37:17 -0500

    Skeleton commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# AoC2024
-2024 Advent of Code
+# aoc2024
+
+(Advent of Code)[https://adventofcode.com] 2024
+
+
+## Usage
+
+`lein run` to run all days, printing results and timing information.
+
+Inputs are expected in the `inputs/` directory.
+
+## License
+
+Copyright &copy; 2024 Eric Kalkman
+
+This project is licensed under the [GNU General Public License v3.0][license].
+
+[license]: https://choosealicense.com/licenses/gpl-3.0
---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject aoc2024 "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2024"
+  :license {:name "GPL-3.0"
+            :url "https://choosealicense.com/licenses/gpl-3.0"
+            :comment "GNU General Public License v3.0"
+            :year 2024
+            :key "gpl-3.0"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot aoc2024.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
