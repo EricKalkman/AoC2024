e9a77da24b1f6a3ffe2eb41fe9955cdf2008aea3
Eric Kalkman <eric.kalkman@pm.me>
2024-12-18 17:11:29 -0500

    Changed some formatting in runner.scm to properly handle Day 18

--- chez/runner.scm
+++ chez/runner.scm
@@ -13,16 +13,16 @@
 
 (define (do-part day p1 p2)
   (format #t "DAY ~a~%" day)
-  (format #t "Part 1: ~2,'0d~%" (time-exec (p1)))
-  (format #t "Part 2: ~2,'0d~%" (time-exec (p2)))
+  (format #t "Part 1: ~a~%" (time-exec (p1)))
+  (format #t "Part 2: ~a~%" (time-exec (p2)))
   (newline))
 
 (define (line-thunks day p1 p2)
-  (let ([lines (file->lines (format "../inputs/day~2,'0d.inp" day))])
+  (let ([lines (file->lines (format #f "../inputs/day~2,'0d.inp" day))])
     (list (λ () (p1 lines)) (λ () (p2 lines)))))
 
 (define (str-thunks day p1 p2)
-  (let ([s (file->string (format "../inputs/day~2,'0d.inp" day))])
+  (let ([s (file->string (format #f "../inputs/day~2,'0d.inp" day))])
     (list (λ () (p1 s)) (λ () (p2 s)))))
 
 (and->> (line-thunks 1 day01:part-1 day01:part-2) (apply do-part 1))
