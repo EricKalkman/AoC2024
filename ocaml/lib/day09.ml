c98c55d0988e2eb9a26548186e0767c3b3511062
Eric Kalkman <eric.kalkman@pm.me>
2024-12-30 18:53:50 -0500

    some tidying

--- ocaml/lib/day09.ml
+++ ocaml/lib/day09.ml
@@ -55,14 +55,14 @@ let checksum chunks =
 
 let move_files_leftward files spaces =
   let open Chunk in
+  (* uses a sorted Set of Chunk.t's to keep track of the leftmost space
+     of a given length and recursively constructs a list of moved files *)
   let rec aux files spaces =
-    (** uses a sorted Set of Chunk.t's to keep track of the leftmost space
-       of a given length and recursively constructs a list of moved files *)
     match files with
     | [] -> []
     | file :: files ->
         let leftmost_space =
-          [ 1; 2; 3; 4; 5; 6; 7; 8; 9 ]  (* 9 options for space lengths *)
+          [ 1; 2; 3; 4; 5; 6; 7; 8; 9 ] (* 9 options for space lengths *)
           (* all space lengths that file fits into *)
           |> List.filter_map (fun len ->
                  if len < file.len || ChunkSet.is_empty spaces.(len) then None
@@ -79,15 +79,16 @@ let move_files_leftward files spaces =
           file :: aux files spaces
         else
           let { pos = spos; len = slen; id = sid } = leftmost_space in
-          let to_add = { id = file.id; pos = spos; len = file.len } in
+          let moved_file = { id = file.id; pos = spos; len = file.len } in
           spaces.(slen) <- ChunkSet.remove leftmost_space spaces.(slen);
           (if slen > file.len then
+            (* shrink the space if there is any remaining *)
              let new_space =
                { pos = spos + file.len; len = slen - file.len; id = sid }
              in
              spaces.(slen - file.len) <-
                ChunkSet.add new_space spaces.(slen - file.len));
-          to_add :: aux files spaces
+          moved_file :: aux files spaces
   in
 
   let spaces_arr = Array.make 10 ChunkSet.empty in
@@ -97,7 +98,9 @@ let move_files_leftward files spaces =
   aux files spaces_arr
 
 let part mover fname =
-  let rev_files, rev_spaces = fname |> Util.slurp |> String.trim |> parse_input in
+  let rev_files, rev_spaces =
+    fname |> Util.slurp |> String.trim |> parse_input
+  in
   mover rev_files (List.rev rev_spaces) |> checksum |> string_of_int
 
 let part_1 = part move_files_blockwise
