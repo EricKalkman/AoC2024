7c6932f85e3b8d2775fe4d426dccc860038138a6
Eric Kalkman <eric.kalkman@pm.me>
2024-12-30 23:50:15 -0500

    Changed intermediate Seqs to Lists

--- ocaml/lib/day10.ml
+++ ocaml/lib/day10.ml
@@ -22,15 +22,15 @@ let part_2 fname =
   let g = fname |> Util.slurp |> Grid.from_string in
   let trailheads =
     (* seq instead of set because we're not deduping this time *)
-    Grid.all_coords_of '0' g |> List.(map (fun x -> [ x ] |> to_seq))
+    Grid.all_coords_of '0' g |> List.(map (fun x -> [ x ]))
   in
   [ '1'; '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9' ]
   |> List.fold_left
        (fun trails elevation ->
          trails
          |> List.map
-              (Seq.concat_map (Ints2.neighbors4 >> List.to_seq)
-              >> Seq.filter (fun c ->
+              (List.concat_map Ints2.neighbors4
+              >> List.filter (fun c ->
                      Grid.(in_bounds g c && get g c == elevation))))
        trailheads
-  |> List.map Seq.length |> Util.sum_list |> string_of_int
+  |> List.map List.length |> Util.sum_list |> string_of_int
