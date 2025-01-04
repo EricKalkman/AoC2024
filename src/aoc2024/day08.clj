f7cdf3e787363c8e42c597af2276cffebf73fffb
Eric Kalkman <eric.kalkman@pm.me>
2025-01-03 20:51:18 -0500

    Day 8

--- src/aoc2024/day08.clj
+++ src/aoc2024/day08.clj
@@ -2,25 +2,25 @@
   (:require [aoc2024.grids :as g]
             [clojure.string :as str]
             [clojure.set :as set]
-            [clojure.math.numeric-tower :as m]))
+            [clojure.math.numeric-tower :as m])
+  (:import [aoc2024.grids vec2 Grid]))
 
-(defn collect-antennae [grid]
-  (->> (g/coords grid)
-       (filter #(not= \. (g/grid-get grid %)))
-       (group-by #(g/grid-get grid %))))
+(defn collect-antennae [^Grid grid]
+  (->> (g/all-coords-of grid #(not= \. ^char %))
+       (group-by grid)))
 
 (defn coords-in-line
   "Calculates all points in grid with integer coordinates on a line from a to b starting from a."
-  [grid a b]
-  (let [[dr dc] (g/coord- b a)
+  [^Grid grid ^vec2 a ^vec2 b]
+  (let [[dr dc] (g/vec2- b a)
         denom (if (or (zero? dr) (zero? dc)) (max-key abs dr dc) (m/gcd (abs dr) (abs dc)))
-        delta [(/ dr denom) (/ dc denom)]]
+        delta (g/->vec2 (/ dr denom) (/ dc denom))]
     (->> a
-         (iterate (fn [coord] (g/coord+ coord delta)))
-         (take-while #(g/in-grid? grid %)))))
+         (iterate (fn [^vec2 coord] (g/vec2+ coord delta)))
+         (take-while #(contains? grid %)))))
 
-(defn first-double-dist [a b coords]
-  (let [[dr dc] (g/coord- b a)
+(defn first-double-dist [^vec2 a ^vec2 b coords]
+  (let [[dr dc] (g/vec2- b a)
         denom (if (or (zero? dr) (zero? dc)) (max-key abs dr dc) (m/gcd (abs dr) (abs dc)))]
     (nth coords (* 2 denom) nil)))
 
@@ -32,25 +32,25 @@
        (take-while #(seq (rest %))) ; while > 1 element
        (mapcat #(map (partial vector (first %)) (rest %)))))
 
-(defn antinodes-of [which grid a b]
+(defn antinodes-of [which ^Grid grid ^vec2 a ^vec2 b]
   ; only take the first frequency (-> antinode is twice the distance from a
   ; as b and vice versa)
   (->>
     (case which
       :first
       (->> [[a b] [b a]]
-           (keep (fn [[x y]] (first-double-dist x y (coords-in-line grid x y)))))
+           (keep (fn [[^vec2 x ^vec2 y]] (first-double-dist x y (coords-in-line grid x y)))))
       :all
       (concat (coords-in-line grid a b) (coords-in-line grid b a)))
     (into #{})))
 
-(defn all-antinodes [which grid as]
+(defn all-antinodes [which ^Grid grid as]
   (->> (combinations as)
        (map #(apply antinodes-of which grid %))
        (reduce set/union #{})))
 
 (defn part [which lines]
-  (let [grid (g/parse-grid lines)
+  (let [grid ^Grid (g/lines->grid lines)
         ants (collect-antennae grid)]
     (->> (vals ants)
          (map #(all-antinodes which grid %))
@@ -61,6 +61,8 @@
 (def part-2 (partial part :all))
 
 (comment
+  (def test-lines (str/split-lines (slurp "inputs/day08.test")))
+
   (part-1 (str/split-lines (slurp "inputs/day08.inp"))) ; 329
   (part-2 (str/split-lines (slurp "inputs/day08.inp"))) ; 1190
 )
